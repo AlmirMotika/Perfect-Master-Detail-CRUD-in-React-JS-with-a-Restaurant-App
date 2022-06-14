192937f7d146e3330fef2e7d9d8e5559b598c82d
Almir Motika <Almir_18B@hotmail.com>
2022-06-14 22:14:02 +0200

    feat(Customer): Get

---
+++ RestaurantAPI/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+
+        public CustomerController(RestaurantDbContext context) { _context = context; }
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+       
+    }
+}
