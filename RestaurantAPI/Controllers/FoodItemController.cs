cdd12b6c5502fc5b69c6103c3e597d788c53bda0
Almir Motika <Almir_18B@hotmail.com>
2022-06-14 21:44:57 +0200

    feat(FoodItem): Get

---
+++ RestaurantAPI/Controllers/FoodItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemController : ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+        public FoodItemController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<FoodItemController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodItem>>> GetFoodItems()
+        {
+            return await _context.FoodItems.ToListAsync();
+        }
+
+    }      
+}
